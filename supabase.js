38c4f7da8d37ab6d89e5a8d197b5e7cb2b502004
mlinktt14 <mlinktt14@gmail.com>
2026-05-28 19:34:01 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+
+<script>
+const SUPABASE_URL = "https://wbcfszivrbuqfqnizarq.supabase.co";
+
+const SUPABASE_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Zzeml2cmJ1cWZxbml6YXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTkyMjEsImV4cCI6MjA5NTQ3NTIyMX0.2iX7Twex3d96A9Iawg3zxvGAjFNLBqt81tSZgrzqA9s";
+
+window.supabaseClient =
+supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+</script>
