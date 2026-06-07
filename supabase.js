365216e846630c72d2489f5a42add313d212220d
mlinktt14 <mlinktt14@gmail.com>
2026-06-07 19:52:19 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js - ENLACE SATELITAL DE CONTROL
 const supabaseUrl = 'https://wbcfszivrbuqfqnizarq.supabase.co'; 
-const supabaseAnonKey = 'TU_CLAVE_ANON_PUBLIC_REAL_AQUÍ'; // <-- Pega aquí tu clave anon de Supabase
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiY2Zzeml2cmJ1cWZxbml6YXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTkyMjEsImV4cCI6MjA5NTQ3NTIyMX0.2iX7Twex3d96A9Iawg3zxvGAjFNLBqt81tSZgrzqA9s'; // <-- Pega aquí tu clave anon de Supabase
 
 if (typeof supabase !== 'undefined') {
     window.supabaseClient = supabase.createClient(supabaseUrl, supabaseAnonKey);
